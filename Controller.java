1a2e3f01fa0f1ce72112f5276d0879926c856ec4
BanachObama <BanachObama@gmail.com>
2013-12-14 17:23:57 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,4 +1,3 @@
-
 package labb4;
 
 import javax.swing.*;
@@ -7,52 +6,93 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Controller extends JPanel implements ActionListener {
-    
+
     private Timer timer;
     private Model model;
     private View view;
     private JFrame frame;
-    
+    private StringBuilder positions;
+    private int time = 0;
+    private static int numOfTimeSteps = 1000;  //Convention?
+    private int i = 0;
+    private BufferedWriter writer;
+
     public static void main(String[] args) {
-        Model model = new Model(10);
+        Model model = new Model(3000);
         new Controller(model, new View(model));
-        
+
     }
-    
+
     public Controller(Model model, View view) {
         super(new BorderLayout());
         this.model = model;
         this.view = view;
+        try {
+            writer = new BufferedWriter(new FileWriter("Positions.txt"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-        JSlider changeL = new JSlider(JSlider.HORIZONTAL,0,10,5);
+        JSlider changeL = new JSlider(JSlider.HORIZONTAL, 0, 10, 5);
         changeL.addChangeListener(new LengthListener());
         this.view.getFrame().add(BorderLayout.SOUTH, changeL);
 
-        
-        JSlider changeD = new JSlider(JSlider.VERTICAL,0,1000,100);
+
+        JSlider changeD = new JSlider(JSlider.VERTICAL, 1, 1000, 100);
         changeD.addChangeListener(new TimeListener());
-        this.view.getFrame().add(BorderLayout.WEST,changeD);
-        timer = new Timer((int)(model.getdt()),this);
+        this.view.getFrame().add(BorderLayout.WEST, changeD);
+        timer = new Timer((int) (model.getdt()), this);
         timer.start();
     }
-    
+
     public class LengthListener implements ChangeListener {
+
         public void stateChanged(ChangeEvent e) {
             JSlider source = (JSlider) e.getSource();
             model.setL(source.getValue());
         }
     }
+
     public class TimeListener implements ChangeListener {
+
         public void stateChanged(ChangeEvent e) {
             JSlider source = (JSlider) e.getSource();
             model.setdt(source.getValue());
-            timer.setDelay((int)model.getdt());
+            timer.setDelay((int) model.getdt());
         }
     }
-    
+
     public void actionPerformed(ActionEvent e) {
         model.updateAll();
         view.repaint();
+        if (i < numOfTimeSteps) {
+            StringBuilder str = new StringBuilder();
+            time += model.getdt();
+            str.append(time);
+            double[] modelPos = model.getPos();
+            for (double pos : model.getPos()) {
+                str.append(",");
+                str.append(pos);
+            }
+            str.append("\n");
+            try {
+                //Effektivare med BufferedWriter
+                writer.write(str.toString());
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            i+=1;
+        }
+        if (i == numOfTimeSteps) {
+            try {
+                writer.close();
+                System.exit(0);              
+            } catch (IOException ex) {
+                System.out.println("Filen kunde inte stängas");
+                ex.printStackTrace();
+            }
+        }
+
     }
-    
 }
