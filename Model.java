6fcae4b4849e6387c6f992634c7758688d3d9029
SirCunningham <lvckv_v@yahoo.se>
2013-12-18 16:26:36 +0100

    kanter

--- Model.java
+++ Model.java
@@ -8,8 +8,8 @@ public class Model {
     private double L = 1;
     private int dt = 100;
     private int regions = 80; //Dela rutan i ett 10x10-rutnät
-    private int xmax = ((int) View.width) - 100;
-    private int ymax = ((int) View.height) - 100;
+    private int xmax = ((int) View.width) - 21;
+    private int ymax = ((int) View.height) - 73;
     private ArrayList<ArrayList<Particle>> stuckParticles =
             new ArrayList<ArrayList<Particle>>();
 
