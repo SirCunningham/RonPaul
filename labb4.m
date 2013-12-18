f03fca872deedd4ab93d1751c84fc8bbde3b9726
SirCunningham <lvckv_v@yahoo.se>
2013-12-18 10:53:07 +0100

    Komprimerar, 5.6.3 återstår

--- labb4.m
+++ labb4.m
@@ -1,53 +1,49 @@
 clear all
 pos = load('Positions.txt');
-t = pos(1:end,1);
-xmin=zeros([1,length(t)]);
-xmax=xmin;
-ymin=xmin;
-ymax=xmin;
-xmean=xmin;
-ymean=xmin;
-xstd=xmin;
-ystd=xmin;
-for(k=1:length(t))
-    xmin(k)=min(pos(k,2:2:end));
-    xmax(k)=max(pos(k,2:2:end));
-    xmean(k)=mean(pos(k,2:2:end));
-    xstd(k)=std(pos(k,2:2:end));
-    ymin(k)=min(pos(k,3:2:end));
-    ymax(k)=max(pos(k,3:2:end));
-    ymean(k)=mean(pos(k,3:2:end));
-    ystd(k)=std(pos(k,3:2:end));
-
-end
+t = pos(:, 1);
+T = length(t);
+null = zeros([1, T]);
+xmin = null; xmax = null; ymin = null; ymax = null;
+xmean = null; ymean = null; xstd = null; ystd = null;
 figure(1)
-for(k=1:length(t))
-    set(gcf,'DoubleBuffer','On')
-    plot(pos(k,2:2:end),pos(k,3:2:end),'*')
+for(k=1:T)
+    x = pos(k, 2:2:end);
+    y = pos(k, 3:2:end);
+    xmin(k) = min(x);
+    xmax(k) = max(x);
+    xmean(k) = mean(x);
+    xstd(k) = std(x);
+    ymin(k) = min(y);
+    ymax(k) = max(y);
+    ymean(k) = mean(y);
+    ystd(k) = std(y);
+
+    set(gcf, 'DoubleBuffer', 'On')
+    plot(x, y, '*')
     axis([min(xmin) max(xmax) min(ymin) max(ymax)])
     drawnow
-    pause(0.01)
+    pause(pos(k, 1))
 end
 
 figure(2)
 hold on
-plot(t,xmin,'r')
-plot(t,ymin,'b')
-plot(t,xmax,'r')
-plot(t,ymax,'b')
-title('Största resp minsta x- och y-värde som funktion av t')
-legend('x','y')
+plot(t, xmin, 'r')
+plot(t, ymin, 'b')
+plot(t, xmax, 'r')
+plot(t, ymax, 'b')
+title('Största resp. minsta x- och y-värde som funktion av t')
+legend('x', 'y')
 
 figure(3)
 hold on
-plot(t,xmean,'r');
-plot(t,ymean,'b');
-title('Medelvärde av x resp y som funktion av t')
-legend('x','y')
+plot(t, xmean, 'r');
+plot(t, ymean, 'b');
+title('Medelvärde för x resp. y som funktion av t')
+legend('x', 'y')
 
 figure(4)
 hold on
-plot(t, xstd,'r')
-plot(t, ystd,'b')
-title('Standardavvikelse av x resp y som funktion av t')
-legend('x','y')
+plot(t, xstd, 'r')
+plot(t, ystd, 'b')
+title('Standardavvikelse för x resp. y som funktion av t')
+legend('x', 'y')
