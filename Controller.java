2609c5f50f1b074e6735869d87e745b9a82f052f
SirCunningham <lvckv_v@yahoo.se>
2013-12-18 12:23:46 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,4 +1,4 @@
-package labb4;
+package java4;
 
 import javax.swing.*;
 import javax.swing.event.*;
@@ -6,66 +6,59 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
+public class Controller extends JPanel implements ActionListener,
+        ChangeListener {
 
-public class Controller extends JPanel implements ActionListener {
-
-    private Timer timer;
-    private Model model;
-    private View view;
-    private JFrame frame;
+    private final Timer timer;
+    private final Model model;
+    private final View view;
+    private final JSlider LSlider;
+    private final JSlider deltaSlider;
+    private static final int maxSteps = 10000;  //Convention?
     private int time = 0;
-    private static int numOfTimeSteps = 10000;  //Convention?
     private int i = 0;
     private BufferedWriter writer;
-
-    public static void main(String[] args) {
-        Model model = new Model(3000);
-        new Controller(model, new View(model));
-
-    }
+    private boolean log;
 
     public Controller(Model model, View view) {
-
         this.model = model;
         this.view = view;
+        log = true;
         try {
             writer = new BufferedWriter(new FileWriter("Positions.txt"));
         } catch (IOException ex) {
+            System.err.println("Kunde inte skapa fil.");
             ex.printStackTrace();
         }
 
-        JSlider changeL = new JSlider(JSlider.HORIZONTAL, 0, 10, 1);
-        changeL.addChangeListener(new LengthListener());
-        this.view.getFrame().add(BorderLayout.SOUTH, changeL);
+        LSlider = new JSlider(JSlider.HORIZONTAL, 0, 10, 1);
+        LSlider.addChangeListener(this);
+        deltaSlider = new JSlider(JSlider.VERTICAL, 0, 200, 20);
+        deltaSlider.addChangeListener(this);
+        this.view.getFrame().add(BorderLayout.NORTH, LSlider);
+        this.view.getFrame().add(BorderLayout.WEST, deltaSlider);
+        this.view.getFrame().pack();
+        this.view.getFrame().setVisible(true);
 
-        JSlider changeD = new JSlider(JSlider.VERTICAL, 0, 200, 20);
-        changeD.addChangeListener(new TimeListener());
-        this.view.getFrame().add(BorderLayout.WEST, changeD);
         timer = new Timer((int) (model.getdt()), this);
         timer.start();
     }
 
-    public class LengthListener implements ChangeListener {
-
-        public void stateChanged(ChangeEvent e) {
-            JSlider source = (JSlider) e.getSource();
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider) e.getSource();
+        if (source == LSlider) {
             model.setL(source.getValue());
         }
-    }
-
-    public class TimeListener implements ChangeListener {
-
-        public void stateChanged(ChangeEvent e) {
-            JSlider source = (JSlider) e.getSource();
+        if (source == deltaSlider) {
             model.setdt(source.getValue());
             timer.setDelay((int) model.getdt());
         }
     }
 
     public void actionPerformed(ActionEvent e) {
-        model.updateAll();
+        model.updatePos();
         view.repaint();
-        if (view.getButton().getState()) {
+        if (view.getButton().getState() == 1 && log) {
             StringBuilder str = new StringBuilder();
             time += model.getdt();
             str.append(time);
@@ -79,21 +72,26 @@ public class Controller extends JPanel implements ActionListener {
             str.append("\n");
             try {
                 writer.write(str.toString());
-
             } catch (IOException ex) {
+                System.err.println("Kunde inte redigera filen.");
                 ex.printStackTrace();
             }
-            i += 1;
-        }
-        if (i == numOfTimeSteps) {
-            try {
-                writer.close();
-                System.exit(0);
-            } catch (IOException ex) {
-                System.out.println("Filen kunde inte stängas");
-                ex.printStackTrace();
+            i++;
+            if (i == maxSteps) {
+                try {
+                    writer.close();
+                    log = false;
+                } catch (IOException ex) {
+                    System.err.println("Kunde inte stänga filen.");
+                    ex.printStackTrace();
+                }
             }
         }
-
     }
+
+    public static void main(String[] args) {
+        Model model = new Model(800);
+        new Controller(model, new View(model));
+    }
+
 }
