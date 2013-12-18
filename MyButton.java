449a3308f3cdaf5fe8a76fd32b541a72108ad827
SirCunningham <lvckv_v@yahoo.se>
2013-12-18 12:25:06 +0100

    Update MyButton.java

--- MyButton.java
+++ MyButton.java
@@ -1,48 +1,42 @@
+package java4;
 
-package labb4;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class MyButton extends JButton implements ActionListener {
-
-    private boolean isClicked = false;
-    private String clicked;
-    private String unClicked;
-    private Color col1;
-    private Color col2;
-
     
-    public void toggleState() {
-        if (isClicked == false) {
-            isClicked = true;
-            this.setText(clicked);
-            this.setBackground(col2);
-        } else {
-            isClicked = false;
-            this.setText(unClicked);
-            this.setBackground(col1);
-        }
+    private int state;
+    private Color[] colArray = new Color[2];
+    private String[] textArray = new String[2];
+    
+    public MyButton(Color col1, Color col2, String text1, String text2) {
+        state = 0;
+        colArray[0] = col1;
+        colArray[1] = col2;
+        textArray[0] = text1;
+        textArray[1] = text2;
+        setOpaque(true);
+        setBackground(colArray[state]);
+        setText(textArray[state]);
     }
-
-    public boolean getState() {
-        return isClicked;
-    }
-
-    public MyButton(Color col1, Color col2, String unClicked, String clicked) {
-        this.col1 = col1;
-        this.col2 = col2;
-        this.clicked = clicked;
-        this.unClicked = unClicked;
-        this.setText(unClicked);
-        this.setBackground(col1);
-    }
-
+    
     public MyButton() {
         this(Color.RED, Color.GREEN, "Loggar inte", "Loggar");
     }
-
+    
+    public void toggleState() {
+        state = (state + 1) % 2;
+        setBackground(colArray[state]);
+        setText(textArray[state]);
+    }
+    
+    public int getState() {
+        return state;
+    }
+    
     public void actionPerformed(ActionEvent e) {
         toggleState();
     }
+
 }
