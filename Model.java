77eaf12a549b6b4794f531006ac4965fc845c8e9
BanachObama <BanachObama@gmail.com>
2013-12-20 10:52:52 +0100

    Update Model.java

--- Model.java
+++ Model.java
@@ -13,7 +13,7 @@ public class Model {
     private static final int radiusSquared = (xmax > ymax ? ymax : xmax)
             * (xmax > ymax ? ymax : xmax) / 25;
     private ArrayList<ArrayList<Particle>> stuckParticles =
-            new ArrayList<>();
+            new ArrayList<ArrayList<Particle>>();
 
     public Model(int n) {
         particles = new Particle[n];
